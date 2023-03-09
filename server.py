83f6e2b2d52cd97a61fec2bf43181cdb77f23a69
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-03-09 15:43:12 -0700

    Homeward bound, more to do

--- server.py
+++ server.py
@@ -22,8 +22,9 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
+
 app = Flask(__name__)
 app.debug = True
 
@@ -74,27 +75,29 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return render_template("index.html")
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    myWorld[entity.key] = entity
+    return myWorld
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return myWorld
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld[entity]
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return myWorld
 
 if __name__ == "__main__":
     app.run()
