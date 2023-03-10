d02d072bf80b9be76756ace8c41ee413071963ef
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-03-10 13:36:51 -0700

    Fixed redirect

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return render_template("index.html")
+    return flask.redirect("./static/index.html")
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
